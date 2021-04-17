e9250b7a15f9069618a247a0e7750a2799dd531d
gy <1290445035@qq.com>
2021-04-17 11:58:43 +0800

    初始环境

--- src/main.js
+++ src/main.js
@@ -5,6 +5,24 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+//mint-ui配置
+import MintUI from "mint-ui"
+import "mint-ui/lib/style.css"
+Vue.use(MintUI)
+
+//axios配置
+import axios from "axios"
+axios.defaults.baseURL="http://127.0.0.1:3000"
+Vue.prototype.axios=axios
+
+//moment配置
+import Moment from "moment"
+Vue.prototype.moment=Moment
+
+//echarts配置
+import Echarts from "echarts"
+Vue.prototype.echarts=Echarts
+
 new Vue({
   router,
   store,
---
+++ src/views/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="index">
+    <mt-header title="diet_diary" fixed></mt-header>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
